7225be433200f419e0af559b566a817b578c0138
Darius104 <comandarius@gmail.com>
2021-11-22 22:54:34 +0100

    Done

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -2,22 +2,17 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
-const userWord = prompt('Inserisci una parola palindroma: ');
-console.log('Parola dell\'utente: ', userWord);
-userWordReverse = [];
+const userWord = prompt('Inserisci una parola: ');
 
-let temp = 0;
-
-// leggo la parola all'incontrario
-    // for( let z = 0; z < userWord.length; z++ ){
-    //     console.log(userWord[z]);
-    // }
-
-    for (let x = userWord.length - 1; x >= 0; x --){
-        console.log(userWord[x]);
-        userWordReverse[temp] = userWord[x];
-
-        temp ++;
+function palindroma (userWord){
+    for( let i = 0; i < userWord.length / 2; i++ ){
+        let current = userWord[i];
+        let index = (userWord.length - i) - 1 ;
+        let last = userWord[index];
+        if ( current != last ){
+            return false;
+        }
     }
-
-console.log(userWordReverse);
+    return true;
+}
+console.log(palindroma(userWord));
